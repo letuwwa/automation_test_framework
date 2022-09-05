dccff942168da06a2c12d4a731d2236f639fb7a1
Litvinenka <letuwwa@gmail.com>
2022-09-05 09:10:54 +0400

    feat: drivers and more

--- test_example.py
+++ test_example.py
@@ -1,2 +1,10 @@
 def test_base() -> None:
     assert True
+
+
+def test_base_driver() -> None:
+    from selenium import webdriver
+    browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
+    browser.get('https://www.linuxhint.com')
+    print('Title: %s' % browser.title)
+    browser.quit()
