b2c220ce5da03cdbd40bdcdfa225b712ad410f3a
Litvinenka <letuwwa@gmail.com>
2022-09-09 20:17:55 +0400

    fix: refactoring

--- test_example.py
+++ test_example.py
@@ -2,9 +2,7 @@ def test_base() -> None:
     assert True
 
 
-def test_base_driver() -> None:
-    from selenium import webdriver
-    browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-    browser.get('https://www.linuxhint.com')
-    print('Title: %s' % browser.title)
-    browser.quit()
+def test_base_driver(selenium) -> None:
+    selenium.get("https://www.linuxhint.com")
+    print(f"Title: {selenium.title}")
+    selenium.quit()
