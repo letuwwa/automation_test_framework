dad4870c7c651325df40287d3ebd88efb9d35aa2
Litvinenka <letuwwa@gmail.com>
2022-09-11 23:36:39 +0400

    feat: BaseElement click_on and _find_element

--- wrapper/base_element.py
+++ wrapper/base_element.py
@@ -1,15 +1,18 @@
 from selenium.webdriver import Chrome, Firefox
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class BaseElement:
-    def __init__(self, driver: Chrome | Firefox):
+    def __init__(self, driver: Chrome | Firefox) -> None:
         self.driver = driver
+        self._driver_wait = WebDriverWait(driver=driver, timeout=5)
 
-    def click_on(self):
-        pass
+    def click_on(self, locator: tuple) -> None:
+        self._find_element(locator=locator).click()
 
-    def move_on(self):
-        pass
-
-    def write_text(self):
-        pass
+    def _find_element(self, locator: tuple) -> WebElement:
+        return self._driver_wait.until(
+            expected_conditions.presence_of_element_located(locator=locator)
+        )
