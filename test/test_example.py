e7fbb1dcd12f6947df1b6170268b1aa2c9ed5b08
Litvinenka <letuwwa@gmail.com>
2022-09-11 23:43:59 +0400

    feat: test BaseElement.click_on

--- test/test_example.py
+++ test/test_example.py
@@ -1,8 +1,23 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from wrapper.base_element import BaseElement
+
+
 def test_base() -> None:
     assert True
 
 
-def test_base_driver(selenium) -> None:
-    selenium.get("https://www.linuxhint.com")
+def test_selenium_fixture(selenium) -> None:
+    selenium.get("https://ubuntu.com/")
     print(f"Title: {selenium.title}")
+    selenium.quit()
+
+
+def test_base_element_click(selenium) -> None:
+    driver = BaseElement(driver=selenium)
+    driver.driver.get(url="https://www.python.org/")
+    about_locator = (By.XPATH, "//*[@id='about']/a")
+    driver.click_on(locator=about_locator)
+    sleep(3)
+    print(f"Title: {driver.driver.title}")
     selenium.quit()
