6847c01d0766635aa6d10e365d29e543deac520f
Litvinenka <letuwwa@gmail.com>
2022-09-17 18:09:25 +0400

    feat: BasePage methods

--- wrapper/base_page.py
+++ wrapper/base_page.py
@@ -1,18 +1,26 @@
-from selenium.webdriver import Chrome, Firefox
-
 from wrapper.base_element import BaseElement
+from selenium.webdriver import Chrome, Firefox
 
 
 class BasePage:
-    def __init__(self, driver: Chrome | Firefox):
+    def __init__(self, driver: Chrome | Firefox) -> None:
         self.driver = driver
         self._element = BaseElement(driver=self.driver)
 
-    def reload(self):
-        pass
+    def reload(self) -> None:
+        self.driver.refresh()
 
-    def close(self):
-        pass
+    def stop_driver(self) -> None:
+        self.driver.quit()
 
-    def open(self):
-        pass
+    def close_current_window(self) -> None:
+        self.driver.close()
+
+    def open(self, url: str) -> None:
+        self.driver.get(url=url)
+
+    def get_current_title(self) -> str:
+        return str(self.driver.title)
+
+    def get_current_url(self) -> str:
+        return str(self.driver.current_url)
